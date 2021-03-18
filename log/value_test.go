12b25eb3790977819d8fbf1ed7ee2ea265e574f5
wangjiehong <wangjiehong@ZBMAC-C02D43U01.local>
2021-03-18 19:57:59 +0800

    wjh: add notes

--- log/value_test.go
+++ log/value_test.go
@@ -5,4 +5,25 @@ import "testing"
 func TestValue(t *testing.T) {
 	logger := With(DefaultLogger, "caller", Caller(4))
 	logger.Print("message", "helloworld")
+	testValues2(logger)
 }
+
+func testValues2(logger Logger) {
+	logger.Print("message", "testValues2")
+	testValues3(logger)
+}
+
+func testValues3(logger Logger) {
+	logger.Print("message", "testValues3")
+	testValues4(logger)
+}
+
+func testValues4(logger Logger) {
+	logger.Print("message", "testValues4")
+	testValues5(logger)
+}
+
+func testValues5(logger Logger) {
+	logger.Print("message", "testValues5")
+}
+
